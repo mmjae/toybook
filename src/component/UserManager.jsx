fff077631a4b3821287a96ad448f9c57090964c0
ma <kohkss@naver.com>
2020-06-15 16:59:46 +0900

    asd

--- src/component/UserManager.jsx
+++ src/component/UserManager.jsx
@@ -25,18 +25,22 @@ class UserManager extends Component {
 
     async getUsersList() {
         var { data: users } = await axios.get(USER_API_BASE_URL + "/users");
+        users.map(user => user.checked = false);
+
         this.setState({ users });
     }
     delUser = (e) => {
         e.preventDefault();
-
-        var checkUser = this.state.checkUser;
-
-        axios.delete(USER_API_BASE_URL + "/delete", { data: checkUser })
+        if(this.state.editMode===true){
+            alert("수정 중엔 회원을 삭제 할 수 없습니다.");
+            return;
+        }
+       var delUserList = this.state.users.filter(user => user.checked===true);
+        console.log(delUserList);
+        axios.delete(USER_API_BASE_URL + "/delete", { data: delUserList })
             .then(res => {
                 this.setState({
-                    users: this.state.users.filter((user) => !checkUser.includes(user.ID)),
-                    checkUser: []
+                    users: this.state.users.filter(user => user.checked===false),
                 });
                 alert(res.data.message);
             }).catch(err => { console.log(err) });
@@ -59,6 +63,8 @@ class UserManager extends Component {
         var result;
         if (this.state.mode === "addOff") {
             result = <AddUser user={function (user) {
+                delete user.message;
+                user.checked=false;
                 this.setState({
                     users: this.state.users.concat(user)
                 });
@@ -70,50 +76,104 @@ class UserManager extends Component {
     }
 
     checkUser = (e) => {
-        const target = e.target;
-        const checked = target.checked;
         const id = e.target.value;
         var idNum = parseInt(id);
+        const target = e.target;
+        const checked = target.checked;
         var arr = this.state.users;
-
-        if (checked === true) {
+        if (checked === false) {
             for (var key in arr) {
                 if (arr[key].ID === idNum) {
+                    var sel = arr[key];
+                    sel.checked = false;
                     this.setState({
-                        checkUser : this.state.checkUser.concat(idNum)
+                        users: arr
                     })
-                }
-            }
-        } else if (checked === false) {
-            for (var k in arr) {
-                if (arr[k].ID === idNum) {
-                    this.setState({
-                        checkUser : this.state.checkUser.splice(idNum, 1)
-                    })
+                    return;
                 }
             }
         }
+        if (checked === true) {
+            for (var k in arr) {
+                if (arr[k].ID === idNum) {
+                    var sel1 = arr[k];
+                    sel1.checked = true;
+                    this.setState({
+                        users: arr
+                    })
+                    return;
+                }
+            }
+        }
+
+
     }
     editUser = (e) => {
         e.preventDefault();
+        var cnt =0 ;
+        var arr = this.state.users;
+
+        for(var key in arr){
+            if(arr[key].checked===true){
+                cnt++;
+            }
+        }
+
+        if(cnt===0){
+            alert("수정할 회원을 선택 해주세요.");
+            return;
+        }
+
+
         this.userList();
-        if(this.state.editMode===true){
+        if (this.state.editMode === true) {
             this.setState({
-                editMode : false
+                editMode: false
             });
-        }else{
+        } else {
             this.setState({
-                editMode : true
+                editMode: true
             })
         }
     }
+
+    editUsers = (e,userID) =>{
+        var name = e.target.name;
+        var value = e.target.value;
+        var arr = this.state.users
+        for(var key in arr){
+            if(arr[key].ID===userID){
+                if(name ==='username'){
+                    arr[key].USERNAME=value
+                }else if(name ==='password'){
+                    arr[key].PASSWORD=value
+                }else if(name ==='age'){
+                    arr[key].AGE=value
+                }
+            } 
+        }
+        this.setState({
+            users : arr
+        });
+
+    }
+
+    cancelcheck = (e) =>{
+        if(e.target.checked===false){
+            e.preventDefault();
+            alert("저장을 먼저 하세요.");
+            return;
+        }
+
+    }
+
 
     userList = () => {
         if (this.state.editMode === false) {
             return (
                 this.state.users.map(user =>
                     <tr key={user.ID}>
-                        <td><input type="checkbox" onChange={this.checkUser} value={user.ID}
+                        <td><input type="checkbox" onChange={this.checkUser} value={user.ID} checked={this.state.users.checked}
                         />&nbsp;&nbsp;{user.ID}</td>
                         <td>{user.USERNAME}</td>
                         <td>{user.PASSWORD}</td>
@@ -122,20 +182,50 @@ class UserManager extends Component {
                 )
             );
         } else {
-            var checkUser = this.state.checkUser;
-            var editUsers = this.state.users.filter((user) => checkUser.includes(user.ID));
-            return(
-            editUsers.map(user =>
-                <tr key={user.ID}>
-                    <td>{user.ID}</td>
-                    <td><input type="text" value={user.USERNAME}/></td>
-                    <td><input type="text" value={user.PASSWORD}/></td>
-                    <td><input type="text" value={user.AGE}/></td>
-                </tr>
-                )
+            return (
+                this.state.users.map(user => {
+                    if (user.checked === true) {
+                        return(<tr key={user.ID}>
+                            <td><input type="checkbox" value={user.ID} checked={this.state.users.checked}
+                            onClick={this.cancelcheck}
+                            />&nbsp;&nbsp;{user.ID}</td>
+                            <td><input type="text" value={user.USERNAME} name="username" onChange={function(e){
+                                this.editUsers(e,user.ID);
+                            }.bind(this)}/></td>
+                            <td><input type="text" value={user.PASSWORD} name='password' onChange={function(e){
+                                this.editUsers(e,user.ID);
+                            }.bind(this)}/></td>
+                            <td><input type="number" value={user.AGE} name='age' onChange={function(e){
+                                this.editUsers(e,user.ID);
+                            }.bind(this)}/></td>
+                        </tr>)
+                    }
+                    return <tr key={user.ID}>
+                        <td><input type="checkbox" onChange={this.checkUser} value={user.ID} checked={this.state.users.checked}
+                        />&nbsp;&nbsp;{user.ID}</td>
+                        <td>{user.USERNAME}</td>
+                        <td>{user.PASSWORD}</td>
+                        <td>{user.AGE}</td>
+                    </tr>
+                })
             )
         }
     }
+
+    editModeCheck =() => {
+        var result;
+        if(this.state.editMode===true){
+            result=<Button variant="outline-warning" onClick={this.editUserSave} style={style1}>저장</Button>
+        }else{
+            result=null;
+        }
+        return result;
+    }
+
+    editUserSave = () => {
+
+    }
+
 
     render() {
 
@@ -162,6 +252,7 @@ class UserManager extends Component {
                         {this.userList()}
                     </tbody>
                 </Table>
+                        {this.editModeCheck()}
             </div>);
     }
 }
@@ -176,5 +267,12 @@ const style = {
     backgroundColor: "#848484",
     color: "white",
     fontWeight: "bold"
+}
+
+const style1 ={
+    color : '#5a6268',
+    fontWeight: "bold",
+    border : "1.5px solid #ffc107",
+    marginLeft:"8px"
 }
 export default UserManager;
